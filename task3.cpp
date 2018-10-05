a40a7f301360cd8f5ccb276e916064b8a389b988
Vlad104 <vlad104khk@gmail.com>
2018-10-05 21:54:05 +0300

    comenting

---
+++ task3.cpp
@@ -0,0 +1,54 @@
+class MyDeck
+{
+public:
+	MyDeck();
+	MyDeck(long deckSize);
+	~MyDeck();
+	
+	void push_front(int const number);
+	void push_back(int const number);
+	int pop_front();
+	int pop_back();
+
+private:
+	long deckSize;
+	int * buf;
+	int head;
+	int tail;
+	bool isDeckEmpty();
+};
+
+MyDeck::MyDeck(long deckSize) {
+	this.deckSize = deckSize;
+}
+
+void MyDeck::push_front(int const number) {
+
+}
+
+
+int main() {
+	int n;
+	std::cin >> n;
+	for (int i = 0; i < n; i++) {
+		int command, number;
+		std::cin >> command >> number;
+		switch (command) {
+			case 1:
+
+				break;
+			case 2:
+			
+				break;
+			case 3:
+			
+				break;
+			case 4:
+			
+				break;
+			default :
+			
+				break;
+		}
+	}
+}
