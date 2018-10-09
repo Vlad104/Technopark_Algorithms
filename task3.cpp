13005b78ad62808d97c07ae90389f75632209055
Vlad104 <vlad104khk@gmail.com>
2018-10-09 17:36:58 +0300

    t3 mem problem

--- task3.cpp
+++ task3.cpp
@@ -1,10 +1,61 @@
+/**
+Дек с динамическим буфером
+Ограничение времени	1 секунда
+Ограничение памяти	5Mb
+Ввод	стандартный ввод или input.txt
+Вывод	стандартный вывод или output.txt
+Реализовать дек с динамическим зацикленным буфером.
+
+Обрабатывать команды push * и pop *.
+
+Формат ввода
+В первой строке количество команд n. n ≤ 1000000.
+
+Каждая команда задаётся как 2 целых числа: a b.
+
+a = 1 - push front
+a = 2 - pop front
+a = 3 - push back
+a = 4 - pop back
+Если дана команда pop *, то число b - ожидаемое значение. 
+Если команда pop * вызвана для пустой структуры данных, то ожидается “-1”.
+
+Пример 1
+Ввод
+3
+1 44
+3 50
+2 44
+Вывод
+YES
+
+Пример 2
+Ввод	
+2
+2 -1
+1 10
+Вывод
+YES
+
+Пример 3
+Ввод	
+2
+3 44
+4 66
+Вывод
+NO
+**/
+
 #include <iostream>
 
 class Deck {
 public:
-	//Deck();
 	Deck(int bufSize);
-	~Deck();
+	~Deck() {delete[] buf_;};
+	Deck(const Deck&) = delete;
+	Deck(Deck&) = delete;
+	Deck& operator=(const Deck&) = delete;
+	Deck& operator=(Deck&) = delete;
 	
 	void pushFront(int const number);
 	void pushBack(int const number);
@@ -23,15 +74,15 @@ private:
 Deck::Deck(int bufSize) {
 	bufSize_ = bufSize;
 	buf_ = new int(bufSize_);
+	head_ = 0;
+	tail_ = 0;
 }
 
 int Deck::size() {
-	if (tail_ > head_) {
-		//return bufSize_ - head_ + tail_;
+	if (tail_ >= head_) {
 		return tail_ - head_;
 	}
 	else {
-		//return tail_ - head_;
 		return bufSize_ - head_ + tail_;
 	}
 }
@@ -47,7 +98,6 @@ void Deck::pushFront(int const number) {
 		buf_ = newBuf;
 		head_ = 0;
 		tail_ = bufSize_;
-		//tail_ = bufSize_ - 1;
 		bufSize_ *= 2;
 	}
 	head_ = (head_ - 1 + bufSize_) % bufSize_;
@@ -66,11 +116,8 @@ void Deck::pushBack(int const number) {
 		buf_ = newBuf;
 		head_ = 0;
 		tail_ = bufSize_;
-		//tail_ = bufSize_ - 1;
 		bufSize_ *= 2;
 	}
-	//buf_[tail_] = number;
-	//tail_ = (tail_ - 1 + bufSize_) % bufSize_;
 	buf_[tail_] = number;
 	tail_ = (tail_ + 1) % bufSize_;
 	return;
@@ -94,6 +141,7 @@ void Deck::clearExtraMemory() {
 
 int Deck::popFront(){
 	if (size() > 0) {
+		std::cout << size() << std::endl;
 		clearExtraMemory();
 		int popNumber = buf_[head_];
 		head_ = (head_ + 1) % bufSize_;
@@ -107,8 +155,6 @@ int Deck::popFront(){
 int Deck::popBack(){
 	if (size() > 0) {
 		clearExtraMemory();
-		//int popNumber = buf_[tail_];
-		//tail_ = (tail_ - 1 + bufSize_) % bufSize_;
 		tail_ = (tail_ - 1 + bufSize_) % bufSize_;
 		int popNumber = buf_[tail_];
 		return popNumber;
@@ -121,7 +167,6 @@ int Deck::popBack(){
 int main() {
 	int n;
 	std::cin >> n;
-	//Deck deck(n);
 	Deck * deck = new Deck(n);
 	bool isCorrect = true;
 	for (int i = 0; i < n; i++) {
@@ -132,9 +177,7 @@ int main() {
 				deck->pushFront(number);
 				break;
 			case 2:
-				temp = deck->popFront();
-				printf("%d == %d\n", temp, number);
-				if (number != temp) {
+				if (number != deck->popFront()) {
 					isCorrect = false;
 				}
 				break;
@@ -142,20 +185,20 @@ int main() {
 				deck->pushBack(number);			
 				break;
 			case 4:
-				temp = deck->popBack();
-				printf("%d == %d\n", temp, number);
-				if (number != temp) {
+				if (number != deck->popBack()) {
 					isCorrect = false;
 				}			
 				break;
 			default :			
 				break;
 		}
-	}
+	}	
+	delete deck;
 	if (isCorrect) {
 		std::cout << "YES" << std::endl;
 	}
 	else {
 		std::cout << "NO" << std::endl;
 	}
+	return 0;
 }
