d1067797c20c1111b1ac4d47c44e94a5782ad323
Vlad104 <vlad104da@gmail.com>
2018-11-09 21:34:31 +0300

    task1 is work

--- Modul_2/task1.cpp
+++ Modul_2/task1.cpp
@@ -1,4 +1,6 @@
 /*
+Задача 1.1
+
 Реализуйте структуру данных типа “множество строк” на основе динамической хеш-таблицы с открытой адресацией. 
 Хранимые строки непустые и состоят из строчных латинских букв. Хеш-функция строки должна быть реализована 
 с помощью вычисления значения многочлена методом Горнера. Начальный размер таблицы должен быть равным 8-ми. 
@@ -20,8 +22,6 @@ i-ая проба g(k, i)=g(k, i-1) + i (mod m). m - степень двойки
 */
 
 
-#include <algorithm>
-#include <forward_list>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -31,26 +31,24 @@ public:
 	explicit HashTable(int size);
 	  ~HashTable() {};
 	  HashTable(const HashTable&) = delete;
-	  //HashTable(HashTable&&) = delete;
+	  HashTable(HashTable&&) = delete;
 	  HashTable& operator=(const HashTable&) = delete;
-	  //HashTable& operator=(&HashTable&&) = delete;
+	  HashTable& operator=(HashTable&&) = delete;
 
 	bool Has(const std::string& key);
 	bool Add(const std::string& key);
 	bool Remove(const std::string& key);
-	void print();
 
  private:
  	int alpha_;
   	int nodeCount_;
-  	int nodeSize_;
   	int rehashLevel_;
   	struct Node {
     	Node() : key(""), deleted(false) {}
 	    std::string key;
 	    bool deleted;
 	    bool isEmpty() {
-	      return key == "";
+	    	return key == "";
 	    }
   	};
   	std::vector<Node> table;
@@ -59,7 +57,7 @@ public:
 };
 
 
-HashTable::HashTable(int size = 8) : table(size), alpha_(23), nodeCount_(0), nodeSize_(size), rehashLevel_(0.75)  {}
+HashTable::HashTable(int size = 8) : table(size), alpha_(23), nodeCount_(0), rehashLevel_(0.75)  {}
 
 int HashTable::Hash(const std::string& key) {
 	int hash = 0;
@@ -70,15 +68,14 @@ int HashTable::Hash(const std::string& key) {
 }
 
 void HashTable::Rehash() {
-	std::vector<Node> temp_table(table); // copy
-	for (auto j : temp_table) {
-		std::cout << j.key << std::endl;
-	}
-	table.erase(table.begin(), table.end());
-	std::cout << "cleared table" << std::endl;
+	std::vector<Node> temp_table(table); // копируем таблицу во временный вектор
 	table.resize(2 * table.size());
-	std::cout << "resized table with size: " << table.size() << std::endl;
-	for (int i = 0; i < table.size(); i++) {
+	for (int i = 0; i < table.size(); i++) { // обнуляем таблицу
+    	Node node;
+    	table[i] = node;
+  	}
+  	nodeCount_ = 0;
+	for (int i = 0; i < temp_table.size(); i++) {
 		if( !temp_table[i].isEmpty() && !temp_table[i].deleted ) {
 			Add(temp_table[i].key);
 		}
@@ -86,37 +83,30 @@ void HashTable::Rehash() {
 }
 
 
-bool HashTable::Has(const std::string& key) { // обдумать
+bool HashTable::Has(const std::string& key) {
 	int hash = Hash(key);
-  	int i = 1;
-  	while ( 1 ) {
-	    if (table[hash].key == key && !table[hash].deleted ) {
+  	int i = 0;
+  	while (true) {
+	    hash = (hash + i*i) % table.size();
+	    if (table[hash].key == key && !table[hash].deleted) {
 	      return true;
 	    }
-	    if (table[hash].isEmpty() && !table[hash].deleted ) {
+	    if ( (table[hash].isEmpty() && !table[hash].deleted) || (i >= table.size()) ) {
 	      return false;
 	    }
-	    if ( i == table.size() ) {
-	      return false;
-	    }
-	    hash = (hash + i*i) % table.size();
 	    i++;
   	}
 }
 
-bool HashTable::Add(const std::string& key) { // обдумать
-	std::cout << "start add" << std::endl;
+bool HashTable::Add(const std::string& key) {
   	if ( nodeCount_ >= table.size() * 0.75) {
-  		std::cout << "start rehash" << std::endl;
   		Rehash();
   	}
 
 	int hash = Hash(key);
-	std::cout << "prev hash for " << key << " is " << hash << std::endl;
   	int i = 0;
-	while ( !table[hash].isEmpty() || table[hash].deleted ) {
+	while (!table[hash].isEmpty() || table[hash].deleted) {
 		hash = (hash + i*i) % table.size();
-		std::cout << "new hash for " << key << " is " << hash << std::endl;
 	    if (table[hash].key == key) {
 	      return false;
 	    }
@@ -129,60 +119,42 @@ bool HashTable::Add(const std::string& key) { // обдумать
 }
 
 
-bool HashTable::Remove(const std::string& key) { // обдумать
+bool HashTable::Remove(const std::string& key) {
 	int hash = Hash(key);
-	int i = 1;
-	while ( 1 ) {
-		if (table[hash].key == key && !table[hash].deleted ) {
+	int i = 0;
+	while (true) {
+		hash = (hash + i*i) % table.size();
+		if (table[hash].key == key && !table[hash].deleted) {
 			table[hash].key = "";
 			table[hash].deleted = true;
 			return true;
 		}
-		if (table[hash].isEmpty() && !table[hash].deleted ) {
+		if ( (table[hash].isEmpty() && !table[hash].deleted) || (i >= table.size()) ) {
 			return false;
 		}
-		if ( i == table.size() ) {
-			return false;
-		}
-		hash = (hash + i*i) % table.size();
 		i++;
 	}
 }
 
-void HashTable::print() {
-	for (int i = 0; i < table.size(); i++) {
-		std::cout << "i = " << i << " ";
-		if (table[i].deleted) {
-			std::cout << "deleted ";
-		}
-		if (table[i].isEmpty()) {
-			std::cout << "empty ";
-		}
-		std::cout << table[i].key << std::endl;
-	}
-}
-
-// not rewrited
 int main() {
-  HashTable strings(3);
-  char command = 0;
-  std::string key;
-  while (std::cin >> command >> key) {
-    switch (command) {
-      case '?':
-        std::cout << (strings.Has(key) ? "OK" : "FAIL") << std::endl;
-        break;
-      case '+':
-        std::cout << (strings.Add(key) ? "OK" : "FAIL") << std::endl;
-        break;
-      case '-':
-        std::cout << (strings.Remove(key) ? "OK" : "FAIL") << std::endl;
-        break;
-      default:
-        std::cerr << "bad input" << std::endl;
-        return 0;
-    }
-    strings.print();
-  }
-  return 0;
+	HashTable table(8);
+	char command = 0;
+	std::string key;
+	while (std::cin >> command >> key) {
+	switch (command) {
+	  case '?':
+	    std::cout << (table.Has(key) ? "OK" : "FAIL") << std::endl;
+	    break;
+	  case '+':
+	    std::cout << (table.Add(key) ? "OK" : "FAIL") << std::endl;
+	    break;
+	  case '-':
+	    std::cout << (table.Remove(key) ? "OK" : "FAIL") << std::endl;
+	    break;
+	  default:
+	    std::cout << "FAIL" << std::endl;
+	    return 0;
+	}
+	}
+	return 0;
 }
