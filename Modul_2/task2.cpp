f55d385edeea3dffe0b4ae0dd318bac82285f492
Vlad104 <vlad104khk@gmail.com>
2018-11-14 18:26:48 +0300

    dz2

--- Modul_2/task2.cpp
+++ Modul_2/task2.cpp
@@ -13,23 +13,24 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <stack>
 
-class BinTree() {
+class BinTree {
 public:
-	BinTree();
+	BinTree() {};
 	BinTree(const BinTree&) = delete;
 	BinTree(BinTree&&) = delete;
 	BinTree& operator=(const BinTree&) = delete;
 	BinTree& operator=(BinTree&&) = delete;
-	~BinTree() {delete[] node;};
+	~BinTree() {delete node;};
 
 	void Add(int key);
-	void PostOrderer();
+	std::vector<int> PostOrderer();
 
 private:
 	struct Node {
 		int key;
-		Npde* left;
+		Node* left;
 		Node* right;
 		explicit Node(const int& key) : key(key), left(nullptr), right(nullptr) {}
 		~Node() {
@@ -37,48 +38,122 @@ private:
 			delete right;
 		}
 	};
-	Node* node;
-}
+	Node* node = nullptr;
+};
 
 void BinTree::Add(int key) {
 
-	BinTree* newNode = new Node(key);
-	if (!node) {
-		node = newNode;
+	if (node == nullptr) {
+		node = new Node(key);
 		return;
 	}
-
-	Node* tempNode = node;
+/*
+	Node* new_node = new Node(key);
     while (true) {
-        if ( key < tempNode->key ) {
-            if (tempNode->left) {
-                tempNode = tempNode->left;
+        if ( key < node->key ) {
+            if (node->left != nullptr) {
+                node = node->left;
             }
             else {
-                tempNode->left = newNode;
-                return;
+                node->left = new_node;
+                break;
             }
         }
         else {
-            if (tempNode->right) {
-                tempNode = tempNode->right;
+            if (node->right != nullptr) {
+                node = node->right;
             }
             else {
-                tempNode->right = newNode;
-                return;
+                node->right = new_node;
+                break;
             }
         }
     }
+    return;
+*/
+
+	Node* new_node = new Node(key);
+	Node* temp_node = node;
+    while (true) {
+        if ( key < temp_node->key ) {
+            if (temp_node->left) {
+                temp_node = temp_node->left;
+            }
+            else {
+                temp_node->left = new_node;
+                break;
+            }
+        }
+        else {
+            if (temp_node->right) {
+                temp_node = temp_node->right;
+            }
+            else {
+                temp_node->right = new_node;
+                break;
+            }
+        }
+    }
+    return;
 
 }
 
-void BinTree::PostOrderer() {
+std::vector<int> BinTree::PostOrderer() {
+	std::vector<int> result;
+	std::stack<Node*> st;
+	Node* prev_node = nullptr;
+	st.push(node);
+	while (!st.empty() && node != nullptr) {
 
+		Node* curr_node = st.top(); 
+
+		if (!prev_node || prev_node->left == curr_node || prev_node->right == curr_node) {
+			if (curr_node->left) {
+				st.push(curr_node->left);
+			}
+			else if (curr_node->right) {
+				st.push(curr_node->right);
+			} else {
+				result.push_back(curr_node->key);
+				st.pop();
+			}
+		}
+		else if (curr_node->left == prev_node) {
+			if (curr_node->right) {
+				st.push(curr_node->right);
+			}
+			else {
+				result.push_back(curr_node->key);
+				st.pop();
+			}
+		}
+		else if (curr_node->right == prev_node) {
+				result.push_back(curr_node->key);
+			st.pop();		
+		}
+		prev_node = curr_node;
+	}
+	return result;
 }
-
 
 
 int main(int argc, char const *argv[]) {
+
+	int N = 0;
+	std::cin >> N;
+	BinTree tree;
+	for (int i = 0; i < N; i++) {
+		int element;
+		std::cin >> element;
+		tree.Add(element);
+	}
+
+
+	auto result = tree.PostOrderer();
+	for (auto key : result) {
+		std::cout << key << " ";
+	}
+	std::cout << std::endl;	
 
 	return 0;
 }
